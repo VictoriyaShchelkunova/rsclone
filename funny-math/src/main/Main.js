1d3e5487498026e318d705065f0140504a275977
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-08 15:08:21 +0300

    feat: add main component

--- funny-math/src/main/Main.js
+++ funny-math/src/main/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './Main.css';
+import { Field } from "../field/Field";
 
 export const Main = () => {
     return (
         <div className="wrapper">
-
+            <Field />
         </div>
     )
 }
