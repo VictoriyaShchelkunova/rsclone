55256a6b3936b90a37c5a00e1924d9a450f87034
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-08 15:07:26 +0300

    feat: add component for rendering body of snake

---
+++ funny-math/src/field/snakeResult/snakeBody/SnakeBody.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./SnakeBody.css";
+
+export const SnakeBody = () => {
+    return (
+        <>
+            <div className="snake-body"></div>
+            <div className="snake-body-second"></div>
+            <div className="snake-body-third"></div>
+            <div className="snake-body-fourth"></div>
+            <div className="snake-body-fifth"></div>
+            <div className="snake-body-sixth"></div>
+            <div className="snake-body-seventh"></div>
+            <div className="snake-body-eighth"></div>
+            <div className="snake-body-ninth"></div>
+            <div className="snake-body-tenth"></div>
+            <div className="snake-body-eleventh"></div>
+        </>
+    )
+}
