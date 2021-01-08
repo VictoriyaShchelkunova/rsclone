651a6fa2f98dac5a1c7a36f199807dbdb758b8ec
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-08 15:00:40 +0300

    feat: add component for rendering results of operation

---
+++ funny-math/src/field/results/Results.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Results.css";
+import { Result } from "./result/Result";
+
+export const Results = () => {
+    return (
+        <div className="results">
+            <Result />
+        </div>
+    )
+}
