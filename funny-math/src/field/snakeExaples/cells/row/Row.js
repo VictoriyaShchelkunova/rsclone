32228ed9abdbe45b00b96a1cc05e5903b5c7610c
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-08 15:05:36 +0300

    feat: add component for rendering row snake game

---
+++ funny-math/src/field/snakeExaples/cells/row/Row.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Row.css";
+import { Cell } from "../cell/Cell";
+
+export const Rows = () => {
+    const rows = [];
+    let indexOperand = 0;
+    let indexOperation = 0;
+    for (let i = 0; i < 5; i++) {
+        rows.push(<div className="row" key={i}>
+            <Cell parity={i} indexOperand={indexOperand} indexOperation={indexOperation}/>
+        </div>);
+        if(!(i % 2)) indexOperand += 4;
+        if(i % 2) indexOperation += 4;
+    }
+    return rows;
+}
