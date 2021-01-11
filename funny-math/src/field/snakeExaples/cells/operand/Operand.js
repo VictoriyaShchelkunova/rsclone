180a2357bf43f1a078001bb469c7957083172e71
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-11 09:15:06 +0300

    feat: add class .droppable for operands

--- funny-math/src/field/snakeExaples/cells/operand/Operand.js
+++ funny-math/src/field/snakeExaples/cells/operand/Operand.js
@@ -3,8 +3,9 @@ import "./Operand.css";
 
 
 export const Operand = ({number}) => {
+   
     return (
-        <div className="operand">
+        <div className="operand droppable" >
             <span>{number}</span>
         </div>
     )
