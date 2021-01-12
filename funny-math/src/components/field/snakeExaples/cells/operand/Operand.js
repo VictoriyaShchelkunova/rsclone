dfab0cfc10f6fd51364bd52e202b36cc55403a65
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-12 11:08:07 +0300

    feat: add functionality changing operand with user's answer

--- funny-math/src/components/field/snakeExaples/cells/operand/Operand.js
+++ funny-math/src/components/field/snakeExaples/cells/operand/Operand.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { firstOperands } from "../cell/getFirstOperands";
 import "./Operand.css";
 
 
-export const Operand = ({number}) => {
-   
+export const Operand = ({number, id, cell}) => {   
+    let operandTextClass;
+    let operandClass;
+    if(id === 0 && cell === 0){ 
+        operandTextClass = "first-operand";
+        operandClass = "operand droppable first-operand"
+    } else {
+        operandTextClass = "text-hidden";
+        operandClass = "operand droppable";
+    }
     return (
-        <div className="operand droppable" >
-            <span>{number}</span>
+        <div className={operandClass} >
+            <span className={operandTextClass}>{number}</span>
         </div>
     )
 }
