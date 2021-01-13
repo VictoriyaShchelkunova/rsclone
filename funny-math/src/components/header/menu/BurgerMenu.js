efb0aeebd9b11cd2d2720f5b95bd2d71e683b92d
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-13 12:50:56 +0300

    feat: add  burger-menu

---
+++ funny-math/src/components/header/menu/BurgerMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./BurgerMenu.css";
+
+export const BurgerMenu = () => {
+    return (
+        <div className="burger-menu">
+            <div className="burger-menu-item"></div>
+            <div className="burger-menu-item"></div>
+            <div className="burger-menu-item"></div>
+        </div>
+    )
+}
