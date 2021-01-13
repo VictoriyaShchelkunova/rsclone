93b5ac2cc0374debbac8d6e184775b6d6282bfbb
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-13 12:47:34 +0300

    feat: add wrapper for snake result block

--- funny-math/src/components/field/snakeResult/SnakeResult.js
+++ funny-math/src/components/field/snakeResult/SnakeResult.js
@@ -4,9 +4,12 @@ import SnakeBody from "./snakeBody/SnakeBody";
 
 export const SnakeResult = () => {
     return (
-        <div className="snake-wrapper">
-            <div className="head-snake"></div>
-            <SnakeBody />
+        <div className="snake-result-block">
+            <div className="snake-wrapper">
+                <div className="head-snake"></div>
+                <SnakeBody />
+            </div>
         </div>
+
     )
 }
