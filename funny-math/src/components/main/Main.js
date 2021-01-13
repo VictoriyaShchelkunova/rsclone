55ac4c657369ffa95db0bf0fc279c2bf4915e265
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-13 12:53:41 +0300

    feat: add header in Main component

--- funny-math/src/components/main/Main.js
+++ funny-math/src/components/main/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Main.css';
 import { Field } from "../field/Field";
+import { Header } from "../header/Header";
 
 export const Main = () => {
     return (
         <div className="wrapper">
+            <Header />
             <Field />
         </div>
     )
