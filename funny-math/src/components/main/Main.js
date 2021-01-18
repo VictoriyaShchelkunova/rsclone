e87f1cbbd9892e8eeef9edd5fbc97389d10f1ec1
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-18 11:04:39 +0300

    feat: add Finish component for rendering in Main component

--- funny-math/src/components/main/Main.js
+++ funny-math/src/components/main/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 import './Main.css';
 import { Field } from "../field/Field";
 import { Header } from "../header/Header";
+import Finish from "../finish/Finish"; 
 
 export const Main = () => {
     return (
         <div className="wrapper">
             <Header />
             <Field />
+            <Finish />
         </div>
     )
 }
