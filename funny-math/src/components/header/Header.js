d65fa66cbdedf3e736be884b49899beab187464e
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-20 11:10:41 +0300

    feat: add functionality to show and hide header block

--- funny-math/src/components/header/Header.js
+++ funny-math/src/components/header/Header.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./Header.css";
-import { BurgerMenu } from "./menu/BurgerMenu";
+import BurgerMenu from "./menu/BurgerMenu";
 import UserData from "./userData/UserData";
+import Menu from "../menu/Menu";
+import { connect } from "react-redux";
 
-export const Header = () => {
+const Header = ({isHeader}) => {
     return (
-        <div className="header">
+        <div className="header" style={isHeader ? {display: "flex"} : {display: "none"}}>
+            <Menu />
             <BurgerMenu />
             <UserData />
         </div>
     )
 }
+
+function mapStateToProps(state){
+    return {
+        isHeader: state.isHeader
+    }   
+}
+
+export default connect(mapStateToProps)(Header);
