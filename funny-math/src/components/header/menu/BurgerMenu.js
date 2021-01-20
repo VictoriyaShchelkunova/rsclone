b3db76a7a1179e1473de5a78483972f28e41f43d
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-20 11:11:19 +0300

    feat: add functionality to show and hide menu

--- funny-math/src/components/header/menu/BurgerMenu.js
+++ funny-math/src/components/header/menu/BurgerMenu.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./BurgerMenu.css";
+import { connect } from "react-redux";
+import { showMenu } from "../../../actions";
 
-export const BurgerMenu = () => {
+export const BurgerMenu = ({showMenu}) => {
+    const showMenuHandler = () => {
+        showMenu();
+    }
     return (
-        <div className="burger-menu">
+        <div className="burger-menu" onClick={showMenuHandler}>
             <div className="burger-menu-item"></div>
             <div className="burger-menu-item"></div>
             <div className="burger-menu-item"></div>
         </div>
     )
 }
+
+function mapStateToProps(state){
+    return {
+        isMenu: state.isMenu,
+    }    
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        showMenu: () => dispatch(showMenu())
+    }
+}
+
+export default connect(mapStateToProps,  mapDispatchToProps)(BurgerMenu)
