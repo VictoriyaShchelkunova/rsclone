4d56d6107d591fa5bf4410fc0a5c57a4b53cf890
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-20 11:11:56 +0300

    feat: add leveles page

---
+++ funny-math/src/components/levelsPage/LevelesPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LevelesPage.css"
+
+export const LevelesPage = () => {
+    return (
+        <section className="leveles-page">
+            <div className="level">EASY</div>
+            <div className="level">MIDDLE</div>
+            <div className="level">HARD</div>
+        </section>
+    )
+}
