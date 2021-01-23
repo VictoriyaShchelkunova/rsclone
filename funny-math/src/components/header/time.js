c7af7be1024ae87c77e1a6ba60e428b43d218927
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-23 11:43:15 +0300

    feat: add functionality to turn off timer

--- funny-math/src/components/header/time.js
+++ funny-math/src/components/header/time.js
@@ -2,40 +2,58 @@ import React, { Component } from "react";
 import Timer from "./Timer";
 
 export default class TimeCounter extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             seconds: 0,
-            minutes: 0
+            minutes: 0,
+            isStopTimer: false,
         }
     }
     componentDidMount() {
-        
-        let timer = setInterval(() => {
-            if(!this.props.isTimer) {
-                clearInterval(timer);
-            } else {
-                let minutes = this.state.minutes;
-                let seconds = this.state.seconds;
-                if(seconds === 59) {
-                    seconds = -1;
-                    minutes += 1;
-                };
-                this.setState(() => {
-                    return {
-                        seconds: seconds + 1,
-                        minutes: minutes,
+        const timer = (() => {
+            setInterval(() => {
+                if (!this.props.isTimer) {
+                    clearInterval(this.timer);
+                    this.setState(() => {
+                        return {
+                           ...this.state,
+                           isStopTimer: true
+                        }
+                    })
+                } else {
+                    if(this.state.isStopTimer){
+                        this.setState(() => {
+                            return {
+                                seconds: 0,
+                                minutes: 0,
+                                isStopTimer: false
+                            }
+                        })
                     }
-                }) 
-            }            
-        }, 1000);
-        
+                    let minutes = this.state.minutes;
+                    let seconds = this.state.seconds;
+                    if (seconds === 59) {
+                        seconds = -1;
+                        minutes += 1;
+                    };
+                    this.setState(() => {
+                        return {
+                            seconds: seconds + 1,
+                            minutes: minutes,
+                        }
+                    })
+                }
+            }, 1000);
+        })()
     }
+
+
 
     render() {
         return (
-            <div className="timer">
-               <Timer minutes={this.state.minutes} seconds={this.state.seconds}/>
+            <div className="timer" style={this.props.isTimer ? { display: "block" } : { display: "none" }}>
+                <Timer minutes={this.state.minutes} seconds={this.state.seconds} />
             </div>
         );
     }
