f0f68479810cda3d6726d716e2badc47ddbacfc1
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-23 11:37:34 +0300

    feat: delete local array data

--- funny-math/src/components/field/snakeExaples/cells/cell/getFirstOperands.js
+++ funny-math/src/components/field/snakeExaples/cells/cell/getFirstOperands.js
@@ -1,62 +1,4 @@
-const data = [
-    {
-        firstOperand: 49,
-        operation: "- 7",
-    },
-    {
-        firstOperand: 42,
-        operation: ": 7",
-
-    },
-    {
-        firstOperand: 6,
-        operation: "x 10"
-
-    },
-    {
-        firstOperand: 60,
-        operation: "+ 21"
-
-    },
-    {
-        firstOperand: 81,
-        operation: "+ 19"
-
-    },
-    {
-        firstOperand: 100,
-        operation: "- 51"
-    },
-    {
-        firstOperand: 49,
-        operation: ": 7"
-    },
-    {
-        firstOperand: 7,
-        operation: "x 8"
-
-    },
-    {
-        firstOperand: 56,
-        operation: "- 20"
-    },
-    {
-        firstOperand: 36,
-        operation: ": 9"
-    },
-    {
-        firstOperand: 4,
-        operation: "x 7"
-    },
-    {
-        firstOperand: 28,
-
-    }
-];
-
-
-
-function getFirstOperands() {
+export function getFirstOperands(data) {
     const operands = data.map(step => step.firstOperand);
     let result = [];
     for (let i = 0; i < 4; i++) {
@@ -81,5 +23,3 @@ function getFirstOperands() {
     }
     return firstOperands;
 }
-
-export const firstOperands = getFirstOperands();
