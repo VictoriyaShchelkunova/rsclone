fe3b5e1e778b2a403bb8a489857d6f5bda0d3c6f
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-23 11:26:54 +0300

    feat: using state in component

--- funny-math/src/components/field/results/Results.js
+++ funny-math/src/components/field/results/Results.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Results.css";
 import Result from "./result/Result";
+import { connect } from "react-redux";
 
-export const Results = () => {
+export const Results = ({isField}) => {
     return (
-        <div className="results">
-            <Result />
+        <div className="results" >
+            <Result isField={isField}/>
         </div>
     )
 }
+
+function mapStateToProps(state){
+    return {
+        isField: state.isField
+    }
+}
+
+export default connect(mapStateToProps)(Results);
+
