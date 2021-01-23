782b7fc7da9773ddeaec33ae9992b1142806fd89
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-01-23 11:38:57 +0300

    feat: delete local array data

--- funny-math/src/components/field/snakeExaples/cells/verticalOperation/getOperations.js
+++ funny-math/src/components/field/snakeExaples/cells/verticalOperation/getOperations.js
@@ -1,62 +1,4 @@
-// import { firstOperands } from "../cell/getFirstOperands";
-
-const data = [
-    {
-        firstOperand: 49,
-        operation: "- 7",
-    },
-    {
-        firstOperand: 42,
-        operation: ": 7",
-
-    },
-    {
-        firstOperand: 6,
-        operation: "x 10"
-
-    },
-    {
-        firstOperand: 60,
-        operation: "+ 21"
-
-    },
-    {
-        firstOperand: 81,
-        operation: "+ 19"
-
-    },
-    {
-        firstOperand: 100,
-        operation: "- 51"
-    },
-    {
-        firstOperand: 49,
-        operation: ": 7"
-    },
-    {
-        firstOperand: 7,
-        operation: "x 8"
-
-    },
-    {
-        firstOperand: 56,
-        operation: "- 20"
-    },
-    {
-        firstOperand: 36,
-        operation: ": 9"
-    },
-    {
-        firstOperand: 4,
-        operation: "x 7"
-    },
-    {
-        firstOperand: 28,
-
-    }
-];
-
-const getOperations = () => {
+export const getOperations = (data) => {
     let verticalOperation = [];
     const horizontalOperation = [];
     for (let i = 0; i < data.length; i++) {
@@ -87,4 +29,3 @@ const getOperations = () => {
     }
 };
 
-export const operations = getOperations();
