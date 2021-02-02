ebf78860c17be5513f2580a15cbc4acdec6a7baf
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2021-02-03 01:50:53 +0300

    feat: add year

--- funny-math/src/components/main/Main.js
+++ funny-math/src/components/main/Main.js
@@ -1,24 +1,25 @@
 import React from "react";
 import './Main.css';
-import  Field  from "../field/Field";
+import Field from "../field/Field";
 import Header from "../header/Header";
-import Finish from "../finish/Finish"; 
+import Finish from "../finish/Finish";
 import Registr from "../registr/Registr";
 import LevelesPage from "../levelsPage/LevelesPage";
 import Statistics from "../statistics/Statistics";
 import { connect } from "react-redux";
 
-export const Main = ({isFinish}) => {
-    
+export const Main = ({ isFinish }) => {
+
     return (
         <div className="wrapper">
             <Header />
             <Field />
-            {isFinish ? <Finish /> : null }
+            {isFinish ? <Finish /> : null}
             <Registr />
             <LevelesPage />
             <Statistics />
             <footer>
+                <span>2021</span>
                 <img src="assets/images/rs_school_js.png" alt="" />
                 <a href="https://rs.school/js/">https://rs.school/js/</a>
                 <a href="https://github.com/VictoriyaShchelkunova/">https://github.com/VictoriyaShchelkunova/</a>
@@ -27,7 +28,7 @@ export const Main = ({isFinish}) => {
     )
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
         isFinish: state.isFinishGame
     }
